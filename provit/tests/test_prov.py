800fdac294478dd522aba83b4eec3eb55ecc582c
Peter Mühleder <peter.muehleder@uni-leipzig.de>
2019-05-22 11:19:54 +0200

    added test for source prov

--- provit/tests/test_prov.py
+++ provit/tests/test_prov.py
@@ -22,6 +22,7 @@ def setup_module(module):
         invalid.write('bla')
 
 def teardown_module(module):
+    #pass
     shutil.rmtree('provit/tests/tmp')
 
 
@@ -46,4 +47,22 @@ def test_invalid_prov_file():
     """    
     with pytest.raises(json.decoder.JSONDecodeError):
         prov = Provenance('provit/tests/tmp/invalid.csv')
+
+def test_add_source_prov():
+    """
+    Test adding files as source
+    """
+
+    prov = Provenance('provit/tests/tmp/test.csv')
+    prov.add(
+        agents=['yada'],
+        activity='testing',
+        description='this is a testfunction'
+    )
+    prov.add_sources(['provit/tests/tmp/source.txt'])
+    prov.save()
+
+    assert len(prov.tree()['sources']) == 1
+    assert prov.tree()['sources'][0]['agent'][0] == 'http://vocab.ub.uni-leipzig.de/provit/provit'
+    assert 'initialize_provit' in prov.tree()['sources'][0]['activity']
 
