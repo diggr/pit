bbe72a55d48fa57b0ba37e385e0d96039f012503
Florian Rämisch <raemisch@ub.uni-leipzig.de>
2019-06-04 16:07:13 +0200

    remove unused code from home

--- provit/home.py
+++ provit/home.py
@@ -11,35 +11,6 @@ from .agent import agent_factory
 
 cfg = get_config()
 
-def _iter_prov_files():
-    """
-    iterate through all prov files in all project directories
-    """
-    for directory in load_directories():
-        for prov in load_prov_files(directory):
-            yield prov
-
-
-def generate_agent_profiles():
-    """
-    iterates through all prov files in the project directories and generates
-    a yaml files for each agent in the prov files
-    """
-    agent_list = {}
-    for prov in _iter_prov_files():
-        new_agents = prov.get_agents(include_primary_sources=True)
-        for slug, data in new_agents.items():
-            if not slug in agent_list:
-                profile = agent_factory(slug, data["type"])
-                agent_list[slug] = profile
-            agent_list[slug].update(data)
-            
-    for slug, profile in agent_list.items():
-        filename = os.path.join(cfg.agents_dir, "{}.yaml".format(slug))
-        with open(filename, "w") as f:
-            yaml.dump(profile.to_json(), f, default_flow_style=False)        
-
-
 def load_directories():
     """
     load the list of directories from the directories yaml file
