c0433bc23ef7ad3148bf6d85732c11b7000c5b17
fa754tuhi <raemisch@ub.uni-leipzig.de>
2019-06-25 09:48:10 +0200

    path_with_directories_and_file fixture > conftest.py
    This fixture was moved, to allow usage in other tests

--- provit/home.py
+++ provit/home.py
@@ -12,11 +12,14 @@ from .agent import agent_factory
 cfg = get_config()
 
 
-def load_directories():
+def load_directories(directories_file=None):
     """
     load the list of directories from the directories yaml file
     """
-    with open(cfg.directories_file) as d_file:
+
+    if not directories_file:
+        directories_file = cfg.directories_file
+    with open(directories_file) as d_file:
         data = yaml.safe_load(d_file)
 
     if not data:
@@ -55,11 +58,11 @@ def remove_directories(directory):
     return dirs
 
 
-def add_directory(directory):
+def add_directory(directory, directories_file=None):
     """
     Add directory to project directories list
     """
-    dirs = load_directories()
+    dirs = load_directories(directories_file)
 
     # if directory already in current list do not add and return current list
     for d in dirs:
@@ -67,14 +70,16 @@ def add_directory(directory):
             return dirs
 
     dirs.append(directory)
-    _save_directories(dirs)
+    _save_directories(dirs, directories_file)
     return dirs
 
 
-def _save_directories(directories):
+def _save_directories(directories, directories_file=None):
     """
     save current state of porject directories list to yaml file
     """
     data = {x["directory"]: {"comment": x["comment"].strip()} for x in directories}
-    with open(cfg.directories_file, "w") as d_file:
+    if not directories_file:
+        directories_file = cfg.directories_file
+    with open(directories_file, "w") as d_file:
         yaml.dump(data, d_file, default_flow_style=False)
